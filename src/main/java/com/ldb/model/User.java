a3f1a5e3cb6c6b3e573f3c9a624046ccfb7fc504
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-03 16:41:11 +0200

    refactored user class to adhere to OOP

--- src/main/java/com/ldb/model/User.java
+++ src/main/java/com/ldb/model/User.java
@@ -1,13 +1,22 @@
 package main.java.com.ldb.model;
 
 public class User {
-    private int id;
+    private Integer id; // Use Integer to allow null for new users
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
-    public User(int id, String firstName, String lastName, String email, String password) {
+    // Constructor for creating a new user (without ID)
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Constructor for retrieving an existing user (with ID)
+    public User(Integer id, String firstName, String lastName, String email, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -15,8 +24,13 @@ public class User {
         this.password = password;
     }
 
-    public int getId() {
+    // Getters and setters for all fields
+    public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -47,20 +61,10 @@ public class User {
         return password;
     }
 
+    // remove password setter for security
     public void setPassword(String password) {
         this.password = password;
     }
 
-    // You can override toString() for debugging or logging purposes
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+    // additional user methods to come
 }
-
