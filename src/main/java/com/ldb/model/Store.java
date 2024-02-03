2b6f4a9052d5e89a32d3d2be2ba7cd55c6c5a5c6
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-03 18:05:44 +0200

    added second constructor for adding new store

--- src/main/java/com/ldb/model/Store.java
+++ src/main/java/com/ldb/model/Store.java
@@ -1,14 +1,31 @@
 package main.java.com.ldb.model;
 
 public class Store {
-    private int userId; 
-    private String name; 
+    private Integer id; // Use Integer to allow for null if not set
+    private int userId;
+    private String name;
 
+    // Constructor for a newly created store (without an ID)
     public Store(int userId, String name) {
         this.userId = userId;
         this.name = name;
     }
 
+    // Constructor for a store retrieved from the database (with an ID)
+    public Store(Integer id, int userId, String name) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+    }
+
+    // Getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public int getUserId() {
         return userId;
@@ -26,3 +43,4 @@ public class Store {
         this.name = name;
     }
 }
+
