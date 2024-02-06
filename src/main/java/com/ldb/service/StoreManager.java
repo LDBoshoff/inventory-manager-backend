012170a086a17859122c7ce4a435a0f01e391ba0
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-06 16:04:01 +0200

    added method to retrieve store via user id

--- src/main/java/com/ldb/service/StoreManager.java
+++ src/main/java/com/ldb/service/StoreManager.java
@@ -2,6 +2,7 @@ package main.java.com.ldb.service;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import main.java.com.ldb.model.Store;
@@ -25,6 +26,7 @@ public class StoreManager {
 
     public boolean createStore(Store newStore) {
         String query = "INSERT INTO stores (user_id, name) VALUES (?, ?)";
+        
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, newStore.getUserId());
             preparedStatement.setString(2, newStore.getName());
@@ -37,4 +39,28 @@ public class StoreManager {
             return false;
         }
     }
+
+    public Store getStoreByUserId(int userId) {
+        String query = "SELECT * FROM stores WHERE user_id = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, userId);
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                int storeUserId = resultSet.getInt("user_id");
+                String name = resultSet.getString("name");
+                
+                Store store = new Store(id, storeUserId, name);
+                return store;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return null; // Return null if no store is found or if there's an exception
+    }
+
 }
