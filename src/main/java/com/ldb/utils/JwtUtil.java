db3be60d3ce136802a192c1d4b96483569ab087d
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-09 19:48:26 +0200

    Refactored to use id as claim instead of email

--- src/main/java/com/ldb/utils/JwtUtil.java
+++ src/main/java/com/ldb/utils/JwtUtil.java
@@ -9,14 +9,14 @@ public class JwtUtil {
     private static final String SECRET = "a5f1c8e6a41d5b9d73e93b4e7f050aad4d5d27373f8010ee5f5b0c8923f0df99"; // Random 256 bit key
 
     // Creates a JSON Web Token using the logged in users email
-    public static String createToken(String email) {
+    public static String createToken(int userId) {
         Algorithm algorithm = Algorithm.HMAC256(SECRET);
         
         Date issuedAt = new Date(); // current date
         Date expiresAt = new Date(issuedAt.getTime() + 24 * 60 * 60 * 1000); // 24 hours in milliseconds 
 
         String token = JWT.create()
-            .withClaim("email", email) 
+            .withClaim("User ID", userId) 
             .withIssuedAt(issuedAt)
             .withExpiresAt(expiresAt) 
             .sign(algorithm);
@@ -34,10 +34,10 @@ public class JwtUtil {
         }
     }
 
-    public static String extractEmailFromToken(String token) {
+    public static Integer extractUserIdFromToken(String token) {
         try {
             DecodedJWT decodedJWT = JWT.decode(token);
-            return decodedJWT.getClaim("email").asString();
+            return decodedJWT.getClaim("User ID").asInt();
         } catch (Exception e) {
             return null;
         }
