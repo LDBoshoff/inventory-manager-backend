3a4dfb14b6c88a1e658426d0a52aec8cab45ec39
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-09 22:05:53 +0200

    added method to retrieve store by id

--- src/main/java/com/ldb/service/StoreManager.java
+++ src/main/java/com/ldb/service/StoreManager.java
@@ -64,6 +64,29 @@ public class StoreManager {
         
         return null; // Return null if no store is found or if there's an exception
     }
+
+    public Store getStoreById(int storeId) {
+        String query = "SELECT * FROM stores WHERE id = ?";
+    
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, storeId);
+    
+            ResultSet resultSet = preparedStatement.executeQuery();
+    
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                int storeUserId = resultSet.getInt("user_id");
+                String name = resultSet.getString("name");
+    
+                Store store = new Store(id, storeUserId, name);
+                return store;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+        return null; // Return null if no store is found or if there's an exception
+    }
     
     public boolean updateStore(Store store) {
         String query = "UPDATE stores SET name = ?, user_id = ? WHERE id = ?";
