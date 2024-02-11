b1dc61df2db84beb85201b78f7577ff67314f588
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-11 12:19:13 +0200

    Added authentication and authorization.

--- src/main/java/com/ldb/controller/StoreHandler.java
+++ src/main/java/com/ldb/controller/StoreHandler.java
@@ -8,6 +8,7 @@ import com.sun.net.httpserver.HttpHandler;
 
 import main.java.com.ldb.model.Store;
 import main.java.com.ldb.service.StoreManager;
+import main.java.com.ldb.utils.JwtUtil;
 import main.java.com.ldb.utils.Response;
 
 public class StoreHandler implements HttpHandler {
@@ -22,36 +23,47 @@ public class StoreHandler implements HttpHandler {
     public void handle(HttpExchange exchange) throws IOException {
         String requestMethod = exchange.getRequestMethod();
 
-        if (exchange.getRequestMethod().equals("OPTIONS")) {  
+        if ("OPTIONS".equals(requestMethod)) {  
             Response.handlePreflight(exchange);  // Handle preflight request
 
-        } else if (requestMethod.equals("GET")) {
+        } else if ("GET".equals(requestMethod)) {
             String path = exchange.getRequestURI().getPath(); // /api/stores/{storeId}
             String[] parts = path.split("/");
             
             if (parts.length > 3) {
-                String storeIdString = parts[3]; // Assuming the URL is /api/stores/{storeId}
-                int storeId = Integer.parseInt(storeIdString);
+                int storeId = 0;
+
+                try {
+                    storeId = Integer.parseInt(parts[3]);
+                } catch (NumberFormatException e) {
+                    Response.sendResponse(exchange, 400, "Invalid store ID");
+                    return;
+                }
+
+                String jwt = JwtUtil.extractJWTfromHeader(exchange);
+                if (!JwtUtil.authenticate(jwt)) {
+                    Response.sendResponse(exchange, 401, "Unauthorized");
+                    return;
+                }
+
+                int userId = JwtUtil.extractUserIdFromToken(jwt);
+
+                if (!storeManager.verifyOwnership(userId, storeId)) {
+                    Response.sendResponse(exchange, 401, "Unauthorized - not owner");
+                }
+
                 Store store = storeManager.getStoreById(storeId);
-                
-                String response = getStoreDetails(store);
-                exchange.sendResponseHeaders(200, response.getBytes().length);
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
+                if (store == null) {
+                    Response.sendResponse(exchange, 404, "Store not found");
+                    return;
+                }
+
+                Response.sendResponse(exchange, 200, store.toString()); 
             } else {
                 Response.sendResponse(exchange, 400, "Invalid request");
             }
         } else {
             Response.sendResponse(exchange, 405, "Method not allowed for this endpoint. Please use HTTP GET method.");
         }
-
     }
-
-    private String getStoreDetails(Store store) {
-        // Your logic to retrieve store details goes here
-        // For simplicity, we're returning a static string
-        return store.toString();
-    }
-    
 }
