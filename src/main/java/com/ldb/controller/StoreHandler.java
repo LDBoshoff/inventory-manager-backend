05c2cac88dab4263dbcfd8302f7f122a71fa6f6a
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-12 13:21:22 +0200

    added method to update store

--- src/main/java/com/ldb/controller/StoreHandler.java
+++ src/main/java/com/ldb/controller/StoreHandler.java
@@ -1,7 +1,13 @@
 package main.java.com.ldb.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -9,6 +15,7 @@ import com.sun.net.httpserver.HttpHandler;
 import main.java.com.ldb.model.Store;
 import main.java.com.ldb.service.StoreManager;
 import main.java.com.ldb.utils.JwtUtil;
+import main.java.com.ldb.utils.Request;
 import main.java.com.ldb.utils.Response;
 
 public class StoreHandler implements HttpHandler {
@@ -62,8 +69,74 @@ public class StoreHandler implements HttpHandler {
             } else {
                 Response.sendResponse(exchange, 400, "Invalid request");
             }
+        
+        } else if ("PUT".equals(requestMethod)) {
+            // Handle store update
+            String path = exchange.getRequestURI().getPath(); // /api/stores/{storeId}
+            String[] parts = path.split("/");
+            
+            if (parts.length > 3) {
+                try {
+                    int storeId = 0;
+                    try {
+                        storeId = Integer.parseInt(parts[3]);
+                    } catch (NumberFormatException e) {
+                        Response.sendResponse(exchange, 400, "Invalid store ID");
+                        return;
+                    }
+
+                    String jwt = JwtUtil.extractJWTfromHeader(exchange);
+                    if (!JwtUtil.authenticate(jwt)) {
+                        Response.sendResponse(exchange, 401, "Unauthorized");
+                        return;
+                    }
+
+                    int userId = JwtUtil.extractUserIdFromToken(jwt);
+                    if (!storeManager.verifyOwnership(userId, storeId)) {
+                        Response.sendResponse(exchange, 401, "Unauthorized - not owner");
+                        return;
+                    }
+
+                    Map<String, String> fieldValues = parseAndValidateFields(exchange.getRequestBody(), new String[]{"name"});
+                    Store updatedStore = new Store(storeId, userId, fieldValues.get("name"));
+                    // Update the store details
+                    boolean updateSuccess = storeManager.updateStore(updatedStore);
+                    if (!updateSuccess) {
+                        Response.sendResponse(exchange, 500, "Failed to update store");
+                        return;
+                    }
+                
+                    Response.sendResponse(exchange, 200, "Store updated successfully");
+                } catch (IllegalArgumentException e) {
+                    Response.sendResponse(exchange, 400, e.getMessage());
+                }
+            } else {
+                Response.sendResponse(exchange, 400, "Invalid request");
+            }
+        
         } else {
-            Response.sendResponse(exchange, 405, "Method not allowed for this endpoint. Please use HTTP GET method.");
+            Response.sendResponse(exchange, 405, "Method not allowed for this endpoint.");
         }
+    }
+
+    private Map<String, String> parseAndValidateFields(InputStream requestBody, String[] requiredFields) throws IOException {
+        if (Request.emptyRequest(requestBody)) {
+            throw new IllegalArgumentException("Bad Request - Empty Request");
+        }
+
+        JSONObject jsonObject = Request.parseJsonRequest(requestBody);
+        if (jsonObject == null) {
+            throw new IllegalArgumentException("JSON EXCEPTION");
+        }
+
+        Map<String, String> fieldValues = new HashMap<>();
+        for (String field : requiredFields) {
+            String value = jsonObject.optString(field);
+            if (Request.isNullOrEmpty(value)) {
+                throw new IllegalArgumentException("Bad Request - Missing or empty required data in JSON request");
+            }
+            fieldValues.put(field, value);
+        }
+        return fieldValues;
     }
 }
