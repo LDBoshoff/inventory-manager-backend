540d3f2bc13aac869cce561fd8632f5d38b8d4d5
LDBoshoff <luhahn.boshoff@gmail.com>
2024-02-17 16:29:35 +0200

    added method to retrieved product by name and store id

--- src/main/java/com/ldb/service/ProductManager.java
+++ src/main/java/com/ldb/service/ProductManager.java
@@ -44,6 +44,33 @@ public class ProductManager {
         }
     }
 
+    public Product getProductByNameAndStoreId(String name, int storeId) {
+        String query = "SELECT * FROM products WHERE name = ? AND store_id = ?";
+        
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, storeId);
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                double price = resultSet.getDouble("price");
+                int quantity = resultSet.getInt("quantity");
+                // Assuming the storeId is the same as the one we queried for, so no need to retrieve it again
+                
+                Product product = new Product(id, name, price, quantity, storeId);
+                return product;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return null; // Return null if no product is found or if there's an exception
+    }
+    
+    
+
     public Product getProductById(int productId) {
         String query = "SELECT * FROM products WHERE id = ?";
         
